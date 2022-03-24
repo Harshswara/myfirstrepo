0d22b6fd8bddcf2331600131231c988b618cc054
harshswara <harshitha.r@ceptes.com>
2022-03-24 17:07:07 +0530

    initial commit

---
+++ force-app/main/default/lwc/demoLWC/demoLWC.js
@@ -0,0 +1,45 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+export default class demoLWC extends LightningElement {
+    showerrorToast() {
+        const event = new ShowToastEvent({
+            title :'Toast error',
+            message:'someunexpected error',
+            variant:'error',
+            mode:'dismissable',
+        });
+        this.dispatchEvent(event);
+    }
+
+    showSuccessToast() {
+        const event = new ShowToastEvent({
+            title: 'Toast Success',
+            message:'operation successful',
+            variant:'success',
+            mode:'dismissable',
+        });
+        this.dispatchEvent(event);
+    }
+
+    showwarningtoast() {
+        const event = new ShowToastEvent({
+            title :'Toast warning',
+            message:
+                'Unexpected Success',
+            variant:'warning',
+           mode:'pester',
+        });
+        this.dispatchEvent(event);
+    }
+
+    showinfo() {
+        const event = new ShowToastEvent({
+            title: 'Toast Info',
+            message:
+                'Info Message',
+            variant:'info',
+             mode:'sticky',
+        });
+        this.dispatchEvent(event);
+    }
+}
